1914d54b4a7701c8a5250d59129b4075a574ef4b
Dylan Curtis <Dcurtisj@gmail.com>
2022-12-06 15:15:03 -0800

    make the click function work

--- index.js
+++ index.js
@@ -1,21 +1,29 @@
 
 // global Var
 const questionList = document.querySelector("#questionList")
+const displayQuestion = document.getElementById("question")
 
 //call back
 
 function displayData (dataObj) {
     const li = document.createElement("li")
     li.innerText = dataObj.question
-    li.addEventListener("click", () => console.log ("click"))
+    // li.addEventListener("click", () => console.log ("click"))
+    li.addEventListener("click", (() => handleClick(dataObj)))
     questionList.appendChild(li)
 
+}
+
+const handleClick = (dataObj) => {
+    // debugger
+    displayQuestion.innerText = dataObj.question
 }
 
 
 const fetchData = () => {
     fetch("http://localhost:3000/surveyQuestions")
     .then((response) => response.json())
+    // .then(data => console.log(data))
     .then(data => data.forEach(displayData))
 }
 
