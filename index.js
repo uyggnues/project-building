60d156c7456304fa3a8b01a2859b790f3c88b15b
Dylan Curtis <Dcurtisj@gmail.com>
2022-12-06 08:15:52 -0800

    add json

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 // global Var
 const questionLIst = document.querySelector("#questionList")
 
+
+
 const fetchData = () => {
-    fetch("https://s3-us-west-2.amazonaws.com/s.cdpn.io/40041/FF3.json")
-    .then(response => response.json())
-    .then(questions => console.log(questions))
+    fetch("./db.json")
+    .then((response) => response.json())
+    .then((data)=> console.log(data))
 }
+
+
 
 fetchData()
